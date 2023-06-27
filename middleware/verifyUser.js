6313ea23034d55dc784f11bb4e481c0f82a30487
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-06-27 14:38:08 +0530

    ascaslkcn

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -2,14 +2,17 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY;
-const verifyUser =async (req, res, next) => {
+const verifyUser = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    res.status(400).send("Please log in again");
+  }
   const token = req.headers.authorization.split(" ")[1];
   if (!token) {
     return res.status(400).send("Not Verified");
   }
 
   try {
-    const verify =await jwt.verify(token, SECRET_KEY);
+    const verify = await jwt.verify(token, SECRET_KEY);
     req.user = verify;
     next();
   } catch (error) {
