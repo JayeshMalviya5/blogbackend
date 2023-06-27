36fd9895456a7007d377ebd2511c29521254da0b
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-06-27 10:40:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const env = require("dotenv");
+const routes = require("./routes/Route.js");
+const { limit } = require("./middleware/rateLimit.js");
+env.config();
+const PORT = process.env.PORT || 8000;
+const URI = process.env.MONGODB_URI;
+
+//connection of mongoDB
+mongoose.connect(URI).then(() => {
+  console.log("mongoDb connected");
+});
+
+const app = express();
+
+// using cors
+app.use(cors());
+
+//bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(limit)
+
+app.use("/", express.static("uploads"), routes);
+
+app.listen(8000, () => {
+  console.log(`Server was connected on PORT ${PORT}`)
+});
